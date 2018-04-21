b137bf8bd28e36361f6aaa59febb067a2d479fea
xiaochicao(曹晓驰) <xiaochicao@tencent.com>
2018-04-21 17:00:56 +0800

    begin go

---
+++ begin/begin.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+import "github.com/go-redis/redis"
+
+func main() {
+	fmt.Printf("hello, world\n")
+	client := redis.NewClient(&redis.Options{
+			Addr: "localhost:6379",
+			Password: "meg00d",
+			DB: 0,
+	})
+
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+}
